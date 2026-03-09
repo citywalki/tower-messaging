cc30a05a652763eb02aba42deb9c665ed3e09d28
walkin <wyc19966@hotmail.com>
2026-03-09 10:53:05 +0800

    🔥 chore: remove old messaging core infrastructure
    Remove legacy core components replaced by interface-based design:
    - MessageBus, DefaultMessageBus, Message, GenericMessage, MetaData
    - Bootstrap, MessageHandle, OperationInvoker, ReflectionInvoker
    - ResponseType hierarchy (InstanceResponseType, ListInstancesResponseType, etc.)
    - Old MessageInterceptor chain
    - CDI integration for old system (CDIMessageBus, CDILookupService, etc.)
    
    All replaced by direct CommandHandler interface usage.

--- messaging-core/src/main/java/io/iamcyw/tower/messaging/MessageBus.java
+++
@@ -1,9 +0,0 @@
-package io.iamcyw.tower.messaging;
-
-import java.util.concurrent.CompletableFuture;
-
-public interface MessageBus {
-
-    <R> CompletableFuture<R> dispatch(Message<R> message);
-
-}
--- messaging-core/src/main/java/io/iamcyw/tower/messaging/handle/helper/MethodInvoker.java
+++
@@ -1,7 +0,0 @@
-package io.iamcyw.tower.messaging.handle.helper;
-
-public interface MethodInvoker {
-
-    <R> R invoke(Object instance, Object[] args);
-
-}
